509c24acb99a9b324b17f9833e047be0b6282f4b
mathesbh <mathesbh@gmail.com>
2021-01-22 10:50:11 -0300

    Modifying url api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from './auth';
 
 const api = axios.create({
-    baseURL : 'http://localhost:3001/',
+    baseURL : 'https://backend-one-on-ones.herokuapp.com/',
 });
 
 api.interceptors.request.use(async config => {

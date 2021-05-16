b04a329f3f95eb3f50a2caace7e927957d22d75d
mathesbh <mathesbh@gmail.com>
2021-05-16 15:55:52 -0300

    Configura exibição data e hora dos encontros

--- src/components/meeting/MeetingList.jsx
+++ src/components/meeting/MeetingList.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Navbar } from '../navbar/Navbar';
 import './meeting.css';
+import ptBR from 'date-fns/locale/pt-BR';
+import { format, parseISO } from 'date-fns';
 
 export const MeetingList = ({ meetings }) => {
+  const handleScheduled = (scheduled) => {
+    const date = parseISO(scheduled)
+    const formattedDate = format(date, "'Dia' dd 'de' MMMM', às ' HH:mm'h'", {locale: ptBR});
+    return formattedDate;
+  }
+
   return (
     <>
       <Navbar />
@@ -13,7 +21,7 @@ export const MeetingList = ({ meetings }) => {
               <div className="box-meeting mx-auto mt-4">
                 <div className="list-meeting text-center pt-2">
                 <h5>Encontro com: {e.meetWith.name}</h5>
-                  <p>Data: {e.scheduledTo}</p>
+                  <p>{handleScheduled(e.scheduledTo)}</p>
                 </div>
               </div>
               ))}

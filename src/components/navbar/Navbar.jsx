2622312363cd2f054b3b6b9bacecc3b9ec575f61
Matheus Salomão <mathesbh@gmail.com>
2021-06-15 20:18:43 -0300

    Corrige class para className

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,8 +5,8 @@ import { logout } from '../../services/auth';
 export const Navbar = () => (
   <nav className="navbar navbar-expand-lg navbar-dark nav-bg">
     <Link to="/encontros" className="navbar-brand">One on One's</Link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mr-auto">

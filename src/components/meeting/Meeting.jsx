3c558dd62d9871fadc90cbdb613afbc853dd7375
Matheus Salomão <mathesbh@gmail.com>
2021-06-15 21:08:00 -0300

    Refatoração componente Meeting

--- src/components/meeting/Meeting.jsx
+++ src/components/meeting/Meeting.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { MeetingList } from './MeetingList';
 import { useMeetings } from '../hooks/useMeetings';
+import './meeting.css';
+import { Navbar } from '../navbar/Navbar';
 
 export default function Meeting() {
     const meetings = useMeetings();
     
     return (
         <>
-            <MeetingList meetings={meetings} />
+            <Navbar />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12">
+                        {meetings.map(m => (
+                            <div key={m._id}>
+                                <MeetingList meetings={m}/>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </>
     )
 }

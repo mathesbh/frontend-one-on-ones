62e05135fce2883d59ecbc5e2fee29e3c54a7cdc
Matheus Salomão <mathesbh@gmail.com>
2021-06-15 22:06:10 -0300

    Remove componente botão

--- src/components/button/Btn.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const Btn = ({ btn, name, ico, id, concluir }) => {
-
-  return (
-    <button onClick={() => concluir(id)} type="button" className={btn}>{name} <i className={`far ${ico}`}></i></button>
-  );
-}
